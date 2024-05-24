8c72e774b786d0171535871c565c0f45aca04721
Caio Adania <caioadania@gmail.com>
2024-05-24 11:03:55 -0300

    update tela

--- consoleXadrez/Program.cs
+++ consoleXadrez/Program.cs
@@ -7,7 +7,9 @@ namespace consoleXadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8,8);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
 
         }
     }
--- consoleXadrez/tabuleiro/Tabuleiro.cs
+++ consoleXadrez/tabuleiro/Tabuleiro.cs
@@ -11,13 +11,18 @@ namespace tabuleiro
     {
         public int Linhas { get; set; }
         public int Colunas { get; set; }
-        private Peca[,] pecas;
+        private Peca[ , ] pecas;
 
         public Tabuleiro(int linhas, int colunas)
         {
             Linhas = linhas;
             Colunas = colunas;
-            pecas = new Peca[Linhas, Colunas];
+            pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
         }
     }
 }
